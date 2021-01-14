490a0b944e5dccfd244411b68924146902467479
sky121 <skylarooz@gmail.com>
2021-01-13 19:02:47 -0800

    part 2

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Sky H on 1/13/21.
+//  Copyright © 2021 Sky H. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
